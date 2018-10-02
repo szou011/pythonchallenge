578015d85039f845ed87596ec59ed393475fa466
Stephen Zou <zou.stephen@gmail.com>
2018-10-02 12:32:46 +0000

    level 03

--- level_03.py
+++ level_03.py
@@ -5,4 +5,12 @@ level_03.py
 import re
 import requests
 
-response = requests.get('http://www.pythonchallenge.com/pc/def/equality.html')
+res = requests.get('http://www.pythonchallenge.com/pc/def/equality.html')
+
+ptn_html = re.compile(r'<!--.*', re.DOTALL)
+
+html = ptn_html.search(res.text).group()
+
+ptn_re = re.compile(r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]')
+
+print(ptn_re.findall(html))
